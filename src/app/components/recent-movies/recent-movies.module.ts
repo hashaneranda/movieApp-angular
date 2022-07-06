7f9295b01b2b7979893dd6b577ad6615b6a58e7c
hashaneranda <hashaneranda@gmail.com>
2022-07-06 14:01:22 +0530

    localization changes added

--- src/app/components/recent-movies/recent-movies.module.ts
+++ src/app/components/recent-movies/recent-movies.module.ts
@@ -1,13 +1,28 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RecentMoviesComponent } from './recent-movies.component';
 import { RouterModule } from '@angular/router';
 import { MovieListModule } from '../movies-list/movie-list.module';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { initApp } from '@app/app.module';
+import { HttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [RecentMoviesComponent],
-  imports: [CommonModule, RouterModule, MovieListModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MovieListModule,
+    TranslateModule.forChild({
+      loader: {
+        provide: APP_INITIALIZER,
+        useFactory: initApp,
+        deps: [HttpClient, TranslateService],
+        multi: true,
+      },
+    }),
+  ],
   exports: [RecentMoviesComponent],
 })
 export class RecentMoviesModule {}
